7cf5862ccc596290ce3cf29d4938507ba75968c3
harryprayiv <harryprayiv@gmail.com>
2024-11-03 13:39:32 -0500

    latest changes.  We are still using Deku wrong!  Deku needs more tutorials and Examples... like this one!

--- src/Inventory.purs
+++ src/Inventory.purs
@@ -3,52 +3,120 @@ module Inventory (app) where
 import Prelude
 
 import BudView (ItemCategory(..), MenuItem(..), StrainLineage(..))
-import Data.Either (Either(..), either)
-import Data.Foldable (for_, traverse_)
-import Data.Maybe (Maybe(..))
-import Data.Tuple (Tuple(..))
+import Data.Either (either)
+import Data.Foldable (for_)
 import Data.Tuple.Nested ((/\))
 import Deku.Control (text_)
-import Deku.Core (Nut, useRef)
-import Deku.DOM (HTMLElement)
+import Deku.Core (Nut)
+import Deku.DOM (HTMLInputElement)
 import Deku.DOM as D
-import Deku.DOM.Attributes (value)
 import Deku.DOM.Attributes as DA
 import Deku.DOM.Listeners as DL
-import Deku.DOM.Self as Self
 import Deku.Do as Deku
-import Deku.Hooks (useDyn, useDynAtBeginning, useState, useState')
+import Deku.Hooks (useDynAtBeginning, useRef, useState)
 import Deku.Toplevel (runInBody)
 import Effect (Effect)
 import Effect.Console (log)
-import FRP.Event.Class ((<|*>))
+import FRP.Poll (Poll)
 import Parsing (runParser)
 import Parsing.String.Basic (intDecimal, number)
 import Web.Event.Event (target)
 import Web.HTML (window)
-import Web.HTML.HTMLInputElement (HTMLInputElement, fromEventTarget, value)
-import Web.HTML.HTMLInputElement as HTMLInput
+import Web.HTML.HTMLInputElement (fromEventTarget, value) as HTMLInput
 import Web.HTML.Window (alert)
-import Web.UIEvent.KeyboardEvent (toEvent)
 
--- Simple CSS for input styling
 inputClass :: String
 inputClass = "border border-gray-300 rounded-md p-2 mb-2"
 
--- Parsing helpers
 parseNumber :: String -> Number
 parseNumber str = either (const 0.0) identity (runParser str number)
 
 parseInt :: String -> Int
 parseInt str = either (const 0) identity (runParser str intDecimal)
 
-guardAgainstEmpty :: (String -> Effect Unit) -> String -> Effect Unit 
+guardAgainstEmpty :: (String -> Effect Unit) -> String -> Effect Unit
 guardAgainstEmpty setField text = do
   if text == ""
     then window >>= alert "This field cannot be empty"
     else setField text
 
--- Main form component to capture user input for creating a new inventory item
+renderTextInputField :: String -> Poll String -> (String -> Effect Unit) -> Boolean -> Nut
+renderTextInputField placeholderText state setter guardEmpty = Deku.do
+  { value: currentValue } <- useDynAtBeginning state
+  D.input
+    [ DA.value_ currentValue
+    , DA.placeholder_ placeholderText
+    , DL.input_ (\evt -> do
+        for_ ((target evt) >>= HTMLInput.fromEventTarget) (\inputEl -> do
+          text <- HTMLInput.value inputEl
+          if guardEmpty then guardAgainstEmpty setter text else setter text
+        )
+      )
+    , DA.klass_ inputClass
+    ]
+    []
+
+renderNumberInputField :: String -> Poll Number -> (Number -> Effect Unit) -> (String -> Number) -> Nut
+renderNumberInputField placeholderText state setter parseFunc = Deku.do
+  { value: currentValue } <- useDynAtBeginning state
+  D.input
+    [ DA.value_ (show currentValue)
+    , DA.placeholder_ placeholderText
+    , DL.input_ (\evt -> do
+        for_ ((target evt) >>= HTMLInput.fromEventTarget) (\inputEl -> do
+          text <- HTMLInput.value inputEl
+          setter (parseFunc text)
+        )
+      )
+    , DA.klass_ inputClass
+    ]
+    []
+
+renderIntInputField :: String -> Poll Int -> (Int -> Effect Unit) -> (String -> Int) -> Nut
+renderIntInputField placeholderText state setter parseFunc = Deku.do
+  { value: currentValue } <- useDynAtBeginning state
+  D.input
+    [ DA.value_ (show currentValue)
+    , DA.placeholder_ placeholderText
+    , DL.input_ (\evt -> do
+        for_ ((target evt) >>= HTMLInput.fromEventTarget) (\inputEl -> do
+          text <- HTMLInput.value inputEl
+          setter (parseFunc text)
+        )
+      )
+    , DA.klass_ inputClass
+    ]
+    []
+
+renderForm :: 
+  { setName :: String -> Effect Unit
+  , name :: Poll String
+  , setBrand :: String -> Effect Unit
+  , brand :: Poll String
+  , setPrice :: Number -> Effect Unit
+  , price :: Poll Number
+  , setQuantity :: Int -> Effect Unit
+  , quantity :: Poll Int
+  , setDescription :: String -> Effect Unit
+  , description :: Poll String
+  , handleSubmit :: Effect Unit
+  } -> Nut
+renderForm { setName, name, setBrand, brand, setPrice, price, setQuantity, quantity, setDescription, description, handleSubmit } =
+  D.div_
+    [ D.div
+        [ renderTextInputField "Name" name setName true
+        , renderTextInputField "Brand" brand setBrand true
+        , renderNumberInputField "Price" price setPrice parseNumber
+        , renderIntInputField "Quantity" quantity setQuantity parseInt
+        , renderTextInputField "Description" description setDescription false
+        , D.button
+            [ DL.click_ (\_ -> handleSubmit)
+            , DA.klass_ "p-2 bg-green-500 text-white rounded-md"
+            ]
+            [ text_ "Create Item" ]
+        ]
+    ]
+
 app :: Effect Unit
 app = runInBody $ Deku.do
   setName /\ name <- useState ""
@@ -71,12 +139,12 @@ app = runInBody $ Deku.do
       quantityValue <- quantityRef
       descriptionValue <- descriptionRef
       let newItem = MenuItem
-            { sort: 0 
-            , sku: "SKU001" 
+            { sort: 0
+            , sku: "SKU001"
             , brand: brandValue
             , name: nameValue
             , price: priceValue
-            , measure_unit: "g" 
+            , measure_unit: "g"
             , per_package: "1"
             , quantity: quantityValue
             , category: Flower
@@ -99,69 +167,11 @@ app = runInBody $ Deku.do
       log ("New Item: " <> show newItem)
       window >>= alert "New inventory item created successfully!"
 
-  -- Rendering the input form
-  D.div_
-    [ D.div
-        [ Deku.do
-            { value: currentName } <- useDynAtBeginning name
-            D.input
-              [ DA.value_ currentName
-              , DA.placeholder_ "Name"
-              , DL.input \evt -> do
-                  for_ ((target evt) >>= HTMLInput.fromEventTarget) (guardAgainstEmpty setName)
-              , DA.klass_ inputClass
-              ]
-              []
-        , Deku.do
-            { value: currentBrand } <- useDynAtBeginning brand
-            D.input
-              [ DA.value_ currentBrand
-              , DA.placeholder_ "Brand"
-              , DL.input \evt -> do
-                  for_ ((target evt) >>= HTMLInput.fromEventTarget) (guardAgainstEmpty setBrand)
-              , DA.klass_ inputClass
-              ]
-              []
-        , Deku.do
-            { value: currentPrice } <- useDynAtBeginning price
-            D.input
-              [ DA.value_ (show currentPrice)
-              , DA.placeholder_ "Price"
-              , DL.input \evt -> do
-                  for_ ((target evt) >>= HTMLInput.fromEventTarget) \inputEl -> do
-                    text <- HTMLInput.value inputEl
-                    setPrice (parseNumber text)
-              , DA.klass_ inputClass
-              ]
-              []
-        , Deku.do
-            { value: currentQuantity } <- useDynAtBeginning quantity
-            D.input
-              [ DA.value_ (show currentQuantity)
-              , DA.placeholder_ "Quantity"
-              , DL.input \evt -> do
-                  for_ ((target evt) >>= HTMLInput.fromEventTarget) \inputEl -> do
-                    text <- HTMLInput.value inputEl
-                    setQuantity (parseInt text)
-              , DA.klass_ inputClass
-              ]
-              []
-        , Deku.do
-            { value: currentDescription } <- useDynAtBeginning description
-            D.input
-              [ DA.value_ currentDescription
-              , DA.placeholder_ "Description"
-              , DL.input \evt -> do
-                  for_ ((target evt) >>= HTMLInput.fromEventTarget) \inputEl -> do
-                    text <- HTMLInput.value inputEl
-                    setDescription text
-              , DA.klass_ inputClass
-              ]
-              []
-        , D.button
-            [ DL.click_ \_ -> handleSubmit
-            , DA.klass_ "p-2 bg-green-500 text-white rounded-md"
-            ]
-            [ text_ "Create Item" ]
-        ]
-    ]
+  renderForm
+    { setName, name
+    , setBrand, brand
+    , setPrice, price
+    , setQuantity, quantity
+    , setDescription, description
+    , handleSubmit
+    }
