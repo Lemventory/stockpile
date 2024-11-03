e43269be401095976355caab919a9ace1528eeca
harryprayiv <harryprayiv@gmail.com>
2024-11-03 12:20:53 -0500

    getting far but still problematic

--- src/Inventory.purs
+++ src/Inventory.purs
@@ -9,8 +9,8 @@ import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import Data.Tuple.Nested ((/\))
 import Deku.Control (text_)
-import Deku.Core (Nut)
-import Deku.DOM (HTMLInputElement, HTMLElement)
+import Deku.Core (Nut, useRef)
+import Deku.DOM (HTMLElement)
 import Deku.DOM as D
 import Deku.DOM.Attributes (value)
 import Deku.DOM.Attributes as DA
@@ -24,10 +24,12 @@ import Effect.Console (log)
 import FRP.Event.Class ((<|*>))
 import Parsing (runParser)
 import Parsing.String.Basic (intDecimal, number)
-import Web.DOM.CharacterData (fromEventTarget)
 import Web.Event.Event (target)
 import Web.HTML (window)
+import Web.HTML.HTMLInputElement (HTMLInputElement, fromEventTarget, value)
+import Web.HTML.HTMLInputElement as HTMLInput
 import Web.HTML.Window (alert)
+import Web.UIEvent.KeyboardEvent (toEvent)
 
 -- Simple CSS for input styling
 inputClass :: String
@@ -40,13 +42,8 @@ parseNumber str = either (const 0.0) identity (runParser str number)
 parseInt :: String -> Int
 parseInt str = either (const 0) identity (runParser str intDecimal)
 
--- Handler to check if an input is empty and show an alert if it is
-guardAgainstEmpty
-  :: (String -> Effect Unit)
-  -> HTMLInputElement ()
-  -> Effect Unit -- Could not match kind Row Type with kind Type
-guardAgainstEmpty setField inputEl = do
-  text <- value inputEl
+guardAgainstEmpty :: (String -> Effect Unit) -> String -> Effect Unit 
+guardAgainstEmpty setField text = do
   if text == ""
     then window >>= alert "This field cannot be empty"
     else setField text
@@ -55,27 +52,36 @@ guardAgainstEmpty setField inputEl = do
 app :: Effect Unit
 app = runInBody $ Deku.do
   setName /\ name <- useState ""
+  nameRef <- useRef "" name
   setBrand /\ brand <- useState ""
-  setCategory /\ category <- useState "Flower"
+  brandRef <- useRef "" brand
   setPrice /\ price <- useState 0.0
+  priceRef <- useRef 0.0 price
   setQuantity /\ quantity <- useState 0
+  quantityRef <- useRef 0 quantity
   setDescription /\ description <- useState ""
+  descriptionRef <- useRef "" description
 
   let
     handleSubmit :: Effect Unit
     handleSubmit = do
+      nameValue <- nameRef
+      brandValue <- brandRef
+      priceValue <- priceRef
+      quantityValue <- quantityRef
+      descriptionValue <- descriptionRef
       let newItem = MenuItem
             { sort: 0 
             , sku: "SKU001" 
-            , brand
-            , name
-            , price
+            , brand: brandValue
+            , name: nameValue
+            , price: priceValue
             , measure_unit: "g" 
             , per_package: "1"
-            , quantity
+            , quantity: quantityValue
             , category: Flower
             , subcategory: ""
-            , description
+            , description: descriptionValue
             , tags: []
             , strain_lineage: StrainLineage
                 { thc: "15%"
@@ -91,7 +97,7 @@ app = runInBody $ Deku.do
                 }
             }
       log ("New Item: " <> show newItem)
-      alert "New inventory item created successfully!"
+      window >>= alert "New inventory item created successfully!"
 
   -- Rendering the input form
   D.div_
@@ -99,45 +105,46 @@ app = runInBody $ Deku.do
         [ D.input
             [ DA.value_ name
             , DA.placeholder "Name"
-            , DL.input \evt -> case fromEventTarget (target evt) of
-                Just inputEl -> guardAgainstEmpty setName inputEl
-                Nothing -> pure unit
+            , DL.input \evt -> do
+                for_ ((target evt) >>= HTMLInput.fromEventTarget) (guardAgainstEmpty setName)
             , DA.klass_ inputClass
             ]
             []
         , D.input
             [ DA.value_ brand
             , DA.placeholder "Brand"
-            , DL.input \evt -> case fromEventTarget (target evt) of
-                Just inputEl -> guardAgainstEmpty setBrand inputEl
-                Nothing -> pure unit
+            , DL.input \evt -> do
+                for_ ((target evt) >>= HTMLInput.fromEventTarget) (guardAgainstEmpty setBrand)
             , DA.klass_ inputClass
             ]
             []
         , D.input
-            [ DA.placeholder "Price"
-            , DA.value_ (show price)
-            , DL.input \evt -> case fromEventTarget (target evt) of
-                Just inputEl -> value inputEl >>= setPrice <<< parseNumber
-                Nothing -> pure unit
+            [ DA.value_ (show price)
+            , DA.placeholder "Price"
+            , DL.input \evt -> do
+                for_ ((target evt) >>= HTMLInput.fromEventTarget) \inputEl -> do
+                  text <- HTMLInput.value inputEl
+                  setPrice (parseNumber text)
             , DA.klass_ inputClass
             ]
             []
         , D.input
-            [ DA.placeholder "Quantity"
-            , DA.value_ (show quantity)
-            , DL.input \evt -> case fromEventTarget (target evt) of
-                Just inputEl -> value inputEl >>= setQuantity <<< parseInt
-                Nothing -> pure unit
+            [ DA.value_ (show quantity)
+            , DA.placeholder "Quantity"
+            , DL.input \evt -> do
+                for_ ((target evt) >>= HTMLInput.fromEventTarget) \inputEl -> do
+                  text <- HTMLInput.value inputEl
+                  setQuantity (parseInt text)
             , DA.klass_ inputClass
             ]
             []
         , D.input
-            [ DA.placeholder "Description"
-            , DA.value_ description
-            , DL.input \evt -> case fromEventTarget (target evt) of
-                Just inputEl -> value inputEl >>= setDescription
-                Nothing -> pure unit
+            [ DA.value_ description
+            , DA.placeholder "Description"
+            , DL.input \evt -> do
+                for_ ((target evt) >>= HTMLInput.fromEventTarget) \inputEl -> do
+                  text <- HTMLInput.value inputEl
+                  setDescription text
             , DA.klass_ inputClass
             ]
             []
