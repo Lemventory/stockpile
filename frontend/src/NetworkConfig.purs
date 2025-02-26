4d9a054697a7c18689fd3c7cb33418ae9c8a9469
harryprayiv <harryprayiv@gmail.com>
2025-02-26 11:41:05 -0500

    changed to LAN ip of test machine

--- frontend/src/NetworkConfig.purs
+++ frontend/src/NetworkConfig.purs
@@ -17,8 +17,8 @@ localConfig =
 -- Replace 192.168.1.X with your actual machine's IP address
 networkConfig :: EnvironmentConfig
 networkConfig =
-  { apiBaseUrl: "http://192.168.8.248:8080"
-  , appOrigin: "http://192.168.8.248:5174"
+  { apiBaseUrl: "http://192.168.8.160:8080"
+  , appOrigin: "http://192.168.8.160:5174"
   }
 
 -- Toggle between configurations
